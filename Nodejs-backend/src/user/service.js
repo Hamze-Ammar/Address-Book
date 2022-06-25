ad6a01ce7cf7d89c0d3249ce0efdae611941ef60
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-06-25 10:32:28 +0300

    Added: service.js User | Nodejs

---
+++ Nodejs-backend/src/user/service.js
@@ -0,0 +1,42 @@
+const User = require('../../model/User');
+const Task = require('../../model/Task');
+
+async function getUsers() {
+  return await User.find();
+  // return await Task.find().populate('user');
+}
+
+async function getById(id) {
+  return await User.findById(id).populate('tasks');
+}
+
+async function addUser(body, hashPassword) {
+  const {
+    name,
+    email,
+    address, // Added hamze
+  } = body;
+
+  const user = new User({
+    name,
+    email,
+    address, // Added hamze
+    password: hashPassword,
+    
+  });
+
+  return await user.save();
+}
+
+async function getByEmail(email) {
+  return await User.findOne({
+    email
+  });
+}
+
+module.exports = {
+  getUsers,
+  getById,
+  addUser,
+  getByEmail,
+}

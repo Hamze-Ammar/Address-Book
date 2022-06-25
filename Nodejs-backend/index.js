07241d2d4530a6b3d5f27c0cf6d97bca433e64fd
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-06-25 10:27:51 +0300

    Added index.js | Nodejs

---
+++ Nodejs-backend/index.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const userRouter = require('./src/user');
+const contactRouter = require('./src/contact/index');
+
+const DB_CONNECT = process.env.DB_CONNECT || "";
+mongoose.connect(
+  DB_CONNECT,
+  () => console.log('connected to db')
+);
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/user', userRouter);
+app.use('/api/contact', contactRouter);
+
+app.listen(3000, () => console.log('Server running'));

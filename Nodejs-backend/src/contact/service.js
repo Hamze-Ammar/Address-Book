37467a4eb1f9a3a089ea53974edc4770a30412a4
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-06-25 10:35:31 +0300

    Added: service.js Contact | Nodejs

---
+++ Nodejs-backend/src/contact/service.js
@@ -0,0 +1,36 @@
+const Contact = require('../../model/Contact');
+const User = require('../../model/User');
+
+
+async function addContact(body) {
+    const {
+      fullName,
+      phoneNumber,
+      relationship,
+      email,
+      location,
+      user,
+    } = body;
+  
+    const contact = new Contact({
+      fullName,
+      phoneNumber,
+      relationship,
+      email,
+      location,
+      user,
+    });
+  
+    return await contact.save();
+  }
+
+  async function getById(user_id) {
+    // projection, in case you want to return certain values
+    return await Contact.find({user: user_id});
+  }
+
+
+  module.exports = {
+    addContact,
+    getById,
+  }

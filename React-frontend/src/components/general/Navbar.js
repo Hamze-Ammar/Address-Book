3263ed77285123fe5af0b234b337d461e5ecdf15
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-06-25 16:14:44 +0300

    Updated: Navbar Component | React

--- React-frontend/src/components/general/Navbar.js
+++ React-frontend/src/components/general/Navbar.js
@@ -1,31 +1,38 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import { FaLaughWink } from 'react-icons/fa';
 
 export default function Navbar(props) {
+  let name = localStorage.getItem("user_name");
+  let greetings = `Hi ${name}!`;
   return (
     <div>
       <div className="topnav">
         <div>
-          <a className="active">
-            AddressBook
-          </a>
-         <Link to="/"><span href="#"> 
-            Show Contacts
-            </span></Link>
-            <Link to= "/admin">
-          <span href="#">Add Contact</span></Link>
+          <a className="active">AddressBook</a>
+          <Link to="/">
+            <span href="#">Show Contacts</span>
+          </Link>
+          <Link to="/add">
+            <span href="#">Add Contact</span>
+          </Link>
           <a href="#"></a>
         </div>
-        <div className="topnav-middle"></div>
+        <div className="topnav-middle">
+          <a>{name &&  greetings}</a>
+          
+        </div>
         <div>
-        <Link to="/login"><span className="topnav-right" href="#">
-            Login
-          </span></Link>
+          <Link to="/login">
+            <span className="topnav-right" href="#">
+              Login
+            </span>
+          </Link>
           <Link to="/register">
-          <span className="topnav-right" href="#">
-            Register
-          </span></Link>
+            <span className="topnav-right" href="#">
+              Register
+            </span>
+          </Link>
           <a className="topnav-right" href="#">
             Logout
           </a>
